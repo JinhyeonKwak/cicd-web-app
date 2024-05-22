15c7d1c44cdae8eda59cdb66790e2283e8b09ef2
jinhyeon <vmflwu123@gmail.com>
2024-05-23 00:41:33 +0900

    intial commit

---
+++ src/main/java/com/inflearn/cicdwebapp/CicdWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.cicdwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inflearn/cicdwebapp/CicdWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.cicdwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
